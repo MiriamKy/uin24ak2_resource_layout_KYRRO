fe666c992f87082b7307a123d6e19f7b67cf40a4
MiriamKy <miri.kristiansen@gmail.com>
2024-01-26 00:00:13 +0100

    Tenkt nytt i js-filen, best practice
    - Skriver knapper, innhold og linker fra ressurser.js til index.html
    - Laget funksjonalitet som gjør at knappene kan klikkes og innholdet oppdateres basert på kategori

--- arkivscript.js
+++ arkivscript.js
@@ -1,40 +1,41 @@
-console.log(resources) //Sjekker om jeg har fått linket opp ressurser. js og arkivscript.js(denne filen) korrekt
 
-let arrayIndex = 0
-let aktivtInnhold = 
-    `<h2>${resources[arrayIndex].category}</h2>
-    <p>${resources[arrayIndex].text}</p>
-    <ul>
-        <li><a href=${resources[arrayIndex].sources[0].url}>${resources[arrayIndex].sources[0].title}</a></li>
-        <li><a href=${resources[arrayIndex].sources[1].url}>${resources[arrayIndex].sources[1].title}</a></li>
-        <li><a href=${resources[arrayIndex].sources[2].url}>${resources[arrayIndex].sources[2].title}</a></li>
-    </ul>`
-
-document.getElementById("aktivtInnhold").innerHTML = aktivtInnhold
-
-document.getElementById("HTML").addEventListener("click", function() {
-    arrayIndex = 0
-    overskrivInnhold()
-})
-document.getElementById("CSS").addEventListener("click", function() {
-    arrayIndex = 1
-    overskrivInnhold()
-})
-document.getElementById("script").addEventListener("click", function() {
-    arrayIndex = 2
-    overskrivInnhold()
-})
-document.getElementById("react").addEventListener("click", function() {
-    arrayIndex = 3
-    overskrivInnhold()
-})
-document.getElementById("sanity").addEventListener("click", function() {
-    arrayIndex = 4
-    overskrivInnhold()
+// Skriver kategoriknappene ved hjelp av nøkkelen category fra resources.js til index.html
+resources.map(knapp => {
+document.getElementById("knapper").innerHTML += `<li><button type="button" onClick="oppdaterInnhold('${knapp.category}')">${knapp.category}</button></li>`
 })
 
-function overskrivInnhold() {
-    console.log(arrayIndex)
-    console.log(aktivtInnhold)
-    document.getElementById("aktivtInnhold").innerHTML = aktivtInnhold
+// Skriver forhåndsvalgt innhold fra resources.js til index.html
+let valgtInnhold = 
+    `<h2>${resources[0].category}</h2>
+    <p>${resources[0].text}</p>`
+document.getElementById("aktivtInnhold").innerHTML = valgtInnhold
+
+resources[0].sources.map(kilde => { 
+    document.getElementById("aktivtInnhold").innerHTML += 
+        `<ul><li><a href=${kilde.url}>${kilde.title}</a></li></ul>`
+    })
+
+// Funksjon som kalles ved onClick-eventet på kategoriknappene
+// Det deklareres en variabel (nyArray) som inneholder kun de objektene (her: det objektet) som har nøkkel category lik parameteren kategori, som får verdien sin fra knappen som ble klikket
+// Variabelen brukes til å skrive ut relevant innhold til HTML-en, i forlond til hvilken knapp som er klikket, fordi den nye arrayen nå kun inneholder det tilhørende objektet
+// Går så igjennom den nye arrayen som er lagret i variabelen nyArray, for å skrive ut de tilhørende linkene som listepunkter
+function oppdaterInnhold(kategori) {
+    //console.log("Klikket" + kategori // Sjekker i konsollen at funskjonen kjører ved klikk, og om parameteren kategori har fått en verdi, og om det er riktig verdi, altså stringen på category-nøkkelen i det objektet/knappen som er klikket på.
+    
+    let nyArray = resources.filter((objekt) => objekt.category === kategori)
+    //console.log(nyArray[0].category) // Sjekker syntsax for hvordan variabelen nyArray skal refereres til for å sende
+        
+    document.getElementById("aktivtInnhold").innerHTML = 
+        `<h2>${nyArray[0].category}</h2>
+        <p>${nyArray[0].text}</p>`
+
+    nyArray[0].sources.map(kilde => { 
+        document.getElementById("aktivtInnhold").innerHTML += 
+            `<ul><li><a href=${kilde.url}>${kilde.title}</a></li></ul>`
+        })
+
+    // Dette var et raskt forsøk på å gjøre knappen som ble klikket hvit, men da mistet jeg de andre knappene, og knappen ble heller ikke hvit, så må jobbe litt mer med det :)
+    /*nyArray.map(hvitKnapp => {
+        document.getElementById("knapper").innerHTML = `<li><button class="hvitKnapp" type="button" onClick="oppdaterInnhold('${hvitKnapp.category}')">${hvitKnapp.category}</button></li>`
+        })*/
 }
